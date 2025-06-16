da2862579f16080caafc4712d1952db22c3a08c2
PSFashions <klkerns80@gmail.com>
2025-06-16 12:40:56 -0400

    Your commit message here

--- server.js.js
+++ server.js.js
@@ -2,14 +2,15 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Serve static files from "public" and "images" folders
-app.use(express.static(path.join(__dirname, 'public')));
+// Serve static files from "images" folder
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
-// Optional: Explicit route for "/"
+// Serve index.html at root
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 const PORT = 5000;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
